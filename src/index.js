e597c5d89fb48dfba98128d6f55bdd977c2377bd
waveinyu <waveinyu@gmail.com>
2023-08-14 10:58:43 +0900

    20230814_yuyeong - 메일 비번 변경

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyparser = require('koa-body');
-const cors = require('@koa/cors');
-const logger = require('koa-logger');
+const Koa = require("koa");
+const Router = require("koa-router");
+const bodyparser = require("koa-body");
+const cors = require("@koa/cors");
+const logger = require("koa-logger");
 
 const app = new Koa();
 const router = new Router();
 
-const indexRouter = require('../routes/index');
-const cargoRouter = require('../routes/cargo');
-const salkRouter = require('../routes/salk');
-const testRouter = require('../routes/test');
-const sugarmanRouter = require('../routes/sugarman');
-const sugarmanGoRouter = require('../routes/sugarmanGo');
+const indexRouter = require("../routes/index");
+const cargoRouter = require("../routes/cargo");
+const salkRouter = require("../routes/salk");
+const testRouter = require("../routes/test");
+const sugarmanRouter = require("../routes/sugarman");
+const sugarmanGoRouter = require("../routes/sugarmanGo");
 
 app.use(async (ctx, next) => {
   try {
@@ -20,9 +20,9 @@ app.use(async (ctx, next) => {
   } catch (err) {
     err.status = err.statusCode || err.status || 500;
     ctx.body = {
-      result: 'fail',
-      code: '500',
-      message: 'invalid JSON format'
+      result: "fail",
+      code: "500",
+      message: "invalid JSON format",
     };
     // ctx.app.emit('error', err, ctx);
   }
@@ -30,22 +30,23 @@ app.use(async (ctx, next) => {
 
 app.use(cors());
 app.use(logger());
-app.use(bodyparser({
-  extendTypes: {
-    json: ['application/x-javascript']
-  }
-}));
+app.use(
+  bodyparser({
+    extendTypes: {
+      json: ["application/x-javascript"],
+    },
+  })
+);
 
-
-router.use('/', indexRouter.routes());
-router.use('/salk', salkRouter.routes());
-router.use('/cargo', cargoRouter.routes());
-router.use('/test', testRouter.routes());
-router.use('/sugarman', sugarmanRouter.routes());
-router.use('/sugarmanGo', sugarmanGoRouter.routes());
+router.use("/", indexRouter.routes());
+router.use("/salk", salkRouter.routes());
+router.use("/cargo", cargoRouter.routes());
+router.use("/test", testRouter.routes());
+router.use("/sugarman", sugarmanRouter.routes());
+router.use("/sugarmanGo", sugarmanGoRouter.routes());
 
 app.use(router.routes());
 //app.use(router.allowedMethods());
 app.listen(80, () => {
-  console.log('server is listening to port 80');
+  console.log("server is listening to port 80");
 });
